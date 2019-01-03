0ab8a21a41e6e82f91f2df02ccc04489f3c061bd
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2019-01-03 00:06:35 -0600

    Remove title from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chumchart-frontend';
-}
+export class AppComponent {}
